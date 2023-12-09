d23f0a08027e54d4d0c4f14f1996e5ba4183b7d8
tabish <tabish.khan72@gmail.com>
2023-12-09 05:21:52 -0500

    Added new login and create login logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     testImplementation("junit:junit:4.13.2")
+    implementation ("org.simpleframework:simple-xml:2.7.1")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.mikhaellopez:circularimageview:3.0.2")
